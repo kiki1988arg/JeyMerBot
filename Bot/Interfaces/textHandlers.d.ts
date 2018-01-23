f937e792fca283ab1cc29d652cfde6b9cc190a33
kiki1988arg <ezequielarakaki@hotmail.com>
2018-01-23 01:16:53 -0300

    js eliminados

--- Bot/Interfaces/textHandlers.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class TextHandler {
-    private _successor;
-    successor: TextHandler;
-    handleRequest(msg: string): void;
-}
--- Bot/Models/message.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="mongoose" />
-import { Document, Schema, Model } from "mongoose";
-import { Imessage } from "@Interfaces/Imessage";
-export interface ImessageModel extends Imessage, Document {
-}
-export declare var messageSchema: Schema;
-export declare const message: Model<ImessageModel>;
