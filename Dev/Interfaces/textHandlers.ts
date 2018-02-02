9369285ae4042a0ffbddc3cf9a53f13465e646ea
kiki1988arg <ezequielarakaki@hotmail.com>
2018-02-02 00:20:39 -0300

    flickerapi

--- Dev/Interfaces/textHandlers.ts
+++ Dev/Interfaces/textHandlers.ts
@@ -1,10 +1,10 @@
 class TextHandler {
-    private _successor: TextHandler;
+    private _successor:any;   
   
     public set successor(successor: TextHandler) {
       this._successor = successor;
-    }
-  
+    }    
+
     public get successor() {
       return this._successor;
     }
