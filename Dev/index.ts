1852448a4487612a41d8c16622ff1e66ed42d83e
kiki1988arg <ezequielarakaki@hotmail.com>
2018-02-03 01:34:40 -0300

    meme api

--- Dev/index.ts
+++ Dev/index.ts
@@ -1,6 +1,7 @@
 import { TwitterHandler } from './Handlers/twitterHandler'
 import { GiHandler } from './Handlers/giHandler'
 import { FlickrHandler } from './Handlers/flickrHandler'
+import { MemeHandler } from './Handlers/MemeHandler'
 // import * as mongoose from 'mongoose';
 // import {message , ImessageModel} from "./Models/message";
 
@@ -8,14 +9,18 @@ import { FlickrHandler } from './Handlers/flickrHandler'
       private twitterHandler:any;
       private giHandler:any;
       private flickrHandler:any;
+      private memeHandler:any;
+
       constructor()
       {
           this.twitterHandler = new TwitterHandler();  
           this.giHandler = new GiHandler(); 
           this.flickrHandler = new FlickrHandler();
+          this.memeHandler = new MemeHandler();
           
           this.twitterHandler.successor = this.giHandler;  
-          this.giHandler.successor = this.flickrHandler;       
+          this.giHandler.successor = this.flickrHandler;  
+          this.flickrHandler.successor = this.memeHandler;     
       }
       public async sendMessage(message:any):Promise<any>
       {
--- Test/jeymerbotTest.ts
+++ Test/jeymerbotTest.ts
@@ -19,20 +19,31 @@ describe('twitter deberia subir una imagen', () => {
     });  
   });
 
-  describe('twitter deberia devolver un json con un link', () => { 
-    it('should return a link',async function() { 
-    this.timeout(5000); 
-    let response = await  jmb.sendMessage(".imagen perro"); 
-    expect(response[0].link).to.equal("http://contextodiario.com/wp-content/uploads/2017/12/que-comen-los-perros.jpg");        
+  // describe('twitter deberia devolver un json con un link', () => { 
+  //   it('should return a link',async function() { 
+  //   this.timeout(5000); 
+  //   let response = await  jmb.sendMessage(".imagen perro"); 
+  //   expect(response[0].link).to.equal("http://contextodiario.com/wp-content/uploads/2017/12/que-comen-los-perros.jpg");        
       
-    });  
-  });
+  //   });  
+  // });
 
   describe('link con una imágen', () => { 
     it('should return a link',async function() { 
     this.timeout(5000); 
     let response = await  jmb.sendMessage(".pic maria__"); 
     expect(response).to.equal("https://farm1.staticflickr.com/507/19740064399_62ba6f45bf.jpg");        
+      
+    });  
+  });
+
+
+  
+  describe('link con un meme', () => { 
+    it('should return a link',async function() { 
+    this.timeout(5000); 
+    let response = await  jmb.sendMessage(".meme 61546, Brace yourselves, the sprint planning session is coming"); 
+    expect(response).to.equal("http://i.imgflip.com/23vfko.jpg");        
       
     });  
   });
